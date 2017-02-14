f0d5117caa5fb6b1f443efd93defe3c986322324
ryan <mail@interwebz.space>
2017-02-14 13:27:02 -0700

    getting started with the theme base

---
+++ header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title><?php bloginfo('title')?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>" media="screen" title="no title" charset="utf-8">
+    <?php wp_head()?>
+  </head>
+<header>
+  <h1> <a href="<?php home_url('/')?>"> <?php bloginfo('name')?></a></h1>
+</header>
+    <div="container">
