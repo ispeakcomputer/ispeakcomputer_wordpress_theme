93e6034f62eada5dc7c8b265af0abb4932c4d380
ryan <mail@interwebz.space>
2017-02-15 16:11:58 -0700

    Added the index.php and the single.php view

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php get_header() ?>
+
+<div class="">
+  <?php while(have_posts()):the_post() ?>
+
+    <h2><?php the_title()? ?></h2>
+    <?php the_content(__('Continue Reading')); ?>
+  <?php endwhile; ?>
+
+</div>
+
+<?php get_footer() ?>
+<?php get_sidebar() ?>
