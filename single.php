3781f707631c6597dd237f8db8ebbfee4fb28a3a
ryan <mail@interwebz.space>
2017-02-17 16:08:09 -0700

    adding single.php to display single posts with expanded content . Using the_content() for this

---
+++ single.php
@@ -0,0 +1,20 @@
+<?php get_header()?>
+
+<div class="posts">
+
+  <?php while(have_posts()):the_post()?>
+   <div class="single_post">
+     <a href="#"><?php the_post_thumbnail( 'medium', array('class' => 'img-circle')); ?></a>
+    <h2><?php the_title()?></h2>
+    <?php the_content(); ?>
+    <a href="<?php echo get_permalink(); ?>"> Read More...</a>
+    <div class="hashtags">
+      <?php the_category() ?>
+    </div>
+  </div>
+  <?php endwhile;?>
+
+  <?php get_sidebar()?>
+</div>
+
+<?php get_footer()?>
