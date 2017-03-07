13dc4e4efdb51f09fd5628fff5d80d04ac9d85cc
ryan <mail@interwebz.space>
2017-03-07 11:02:11 -0700

    Added the header links pointing to the correct areas

--- header.php
+++ header.php
@@ -9,15 +9,15 @@
 
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>" media="screen" title="no title" charset="utf-8">
     <?php wp_head()?>
-      
+
   </head>
 <header>
   <div class="my_header">
     <h1 class="toptitle">I SPEAK COMPUTER</h1>
 <div class="subtitle"><p>by</p><h3> <strong>RYAN</strong>WRIGHT</h3></div>
 <ul>
-  <li><a href = "http://www.ispeakcomputer.com"> HOME</a></li>
-  <li><a href = "http://www.ispeakcomputer.com/resume/Ryan_Wright_Resume.pdf" >RESUME</a></li>
+  <li><a href = "http://www.ispeakcomputer.com/blog/"> HOME</a></li>
+  <li><a href = "http://www.ispeakcomputer.com" >RESUME</a></li>
   <li><a href = "http://www.github.com/needsMoreCoffee" >GITHUB</a></li>
 </ul>
 <hr>
