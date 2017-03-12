91cbfed47ed0c588a04a54de893705cfccc53ac2
khole <mail@interwebz.space>
2017-03-12 01:00:01 -0700

    Completed a 404 page

---
+++ 404.php
@@ -0,0 +1,23 @@
+<?php get_header()?>
+
+<div class="posts">
+
+
+   <div id="giphy">
+     <h1>Go Home, You're Lost</h1>
+     <p><img class="entry-content" src="wp-content/themes/instablog/icons/gohome.gif" alt="" /></p>
+  </div>
+  <hr>
+</div>
+
+<div class="navi">
+  <!-- <div class="innernavi"> -->
+<ul>
+
+<li> <a href="/blog">  <img src="wp-content/themes/instablog/icons/home.png" /></a></li>
+
+  </ul>
+  <!-- </div> -->
+</div>
+
+<?php get_footer()?>
