6d1428cad4552ab60635b60b60b7bea8ea16c13d
khole <mail@interwebz.space>
2017-09-05 21:23:10 -0600

    updating subtitle to just use first name

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 <header>
   <div class="my_header">
     <h1 class="toptitle">I SPEAK COMPUTER</h1>
-<div class="subtitle"><p>by</p><h3> <strong>RYAN</strong>WRIGHT</h3></div>
+<div class="subtitle"><p>by</p><h3>RYAN</h3></div>
 <ul>
   <li><a href = "http://www.ispeakcomputer.com/blog/"> HOME</a></li>
   <li><a href = "http://www.ispeakcomputer.com" >RESUME</a></li>
